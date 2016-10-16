64f04f66635196c745b3dfd60e35ed68d4330434
melwinalm <melwinalm@outlook.com>
2016-10-16 11:03:42 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -26,8 +26,6 @@ $(document).ready(function () {
     }
     $("#seats").html(contentString);
 
-
-
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             $('#seats').on('click', '#seat' + i + j, function () {
